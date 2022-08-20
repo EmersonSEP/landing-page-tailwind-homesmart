bf8ee78f685fa9ab5de89e4d64fe3a3b3945c2ad
Antonio Emerson <emerson.nessa@gmail.com>
2022-08-19 23:19:26 -0300

    ajuste no titulo

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
+    <>
+        <h1>Teste</h1>
     <div className="max-w-[2000px] mx-auto text-neutral-900 dark:text-neutral-900 bg-white dark:bg-neutral-800">
       <Header />
     </div>
+    </>
+
   );
 }
 
